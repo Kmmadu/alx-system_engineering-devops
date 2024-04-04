d14ca242086746a2c34d9b31034cc0385dfec517
kmmadu <mmadubugwukingsley@gmail.com>
2024-04-04 22:49:09 +0100

    Finally

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,24 @@
-# Setup nginx server
+# Setup New Ubuntu server with nginx
+
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
 
 package { 'nginx':
-  ensure => 'installed',
+	ensure => 'installed',
+	require => Exec['update system']
 }
 
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
 }
 
-file_line { 'redirect_me':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => '    location /redirect_me/ { return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4; }',
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
 }
-
